3f15e48c338d02618bf57a9e29e562f1440edd50
Igorio29 <igorjosefarias3@gmail.com>
2024-10-15 23:31:33 -0300

    Parte visual completa

--- static/script.js
+++ static/script.js
@@ -10,3 +10,45 @@ menuMobile.addEventListener('click', () => {
         body.classList.toggle("menu-nav-active")
 })
 
+const item = document.querySelectorAll("[data-anime]");
+
+const animeScroll = () => {
+    const windowTop = window.pageYOffset + window.innerHeight * 0.85 ;
+    item.forEach((element) => {
+        if (windowTop > element.offsetTop) {
+            element.classList.add("animate");
+        } else {
+            element.classList.remove("animate");
+        }
+    })
+}
+
+animeScroll()
+
+window.addEventListener("scroll", ()=>{
+    animeScroll()
+});
+
+    // Seleciona todos os links da barra lateral
+    const sidebarLinks = document.querySelectorAll('#navbar a');
+
+    // Função para adicionar 'active' no link correto
+    function updateActiveLink() {
+        let fromTop = window.scrollY + 50;
+
+        sidebarLinks.forEach(link => {
+            let section = document.querySelector(link.getAttribute('href'));
+            
+            // Verifica se a seção está no viewport
+            if (
+                section.offsetTop <= fromTop &&
+                section.offsetTop + section.offsetHeight > fromTop
+            ) {
+                sidebarLinks.forEach(item => item.classList.remove('active')); // Remove active de todos
+                link.classList.add('active'); // Adiciona active ao link atual
+            }
+        });
+    }
+
+    // Atualiza a barra lateral conforme o scroll
+    window.addEventListener('scroll', updateActiveLink);
